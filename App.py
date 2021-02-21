fe5cce28331134011a4cb1fdead60fd5497aad74
komal <sinhakomal40@gmail.com>
2021-02-21 12:14:06 +0530

    commit for app.py

--- App.py
+++ App.py
@@ -1,1 +1,12 @@
 import tkinter as tk
+from tkinter import *
+from tkinter import tkk
+import webbrowser
+from tkinter import messagebox as msgbox
+import datetime
+import os
+
+#Graphic User Interface
+top=tk.Tk()
+top.title('Application')
+top.configure(background='white')
